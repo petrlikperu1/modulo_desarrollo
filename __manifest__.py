0accd5556c57d2296f6191c671d60a1268abc3aa
Ivan <ivanpetrlikazabache@gmail.com>
2020-04-18 18:03:27 -0500

    Segunda Version Terminal 02
    Este  avance comprende del terminal del Ingeniero Ivan Petrlik Azabache

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'modulo_desarrollo',
+    'descripcion':'Modulo de ejemplo 3',
+    'version':'13.0.0.0.0',
+    'author':'Ivan Petrlik',
+    'data': [],
+    'depends':[],
+    'installable': True,
+    'application': True,
+    'auto_install':False,
+}
